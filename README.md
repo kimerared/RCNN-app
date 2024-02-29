2eb008253a32216817866149a49df99146c58f9c
Hector Gavilanes <34449129+kimerared@users.noreply.github.com>
2024-02-28 23:42:29 -0500

    Uploading requirements

--- README.md
+++ README.md
@@ -5,8 +5,16 @@ sdk: gradio
 sdk_version: 4.19.2
 ---
 
+# RCNN Object detection, and segmentation using YOLOv8.
 
-# RCNN Object detection, and segmentation web application using YOLOv8.
-Gradio was used for web deployment.
+### Gradio was used for front-end web development.
 
-Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
+## How to Use the Interface
+
+1.  **Upload Image:** Click on 'Upload Image' to choose an image file for object detection.
+2.  **Adjust Parameters:**
+
+-   **Confidence Threshold:** Slider to set the minimum confidence level for detecting objects.
+-   **IoU Threshold:** Slider to set the IoU threshold for distinguishing different objects.
+
+3.  **View Results:** The processed image with detected objects and their labels will be displayed.
--- app.py
+++ app.py
@@ -1,95 +1,40 @@
-# -*- coding: utf-8 -*-
-"""
-Original file is located at
-    https://colab.research.google.com/drive/1RJuq-k6UqiiawLttbKAD6tTkWAXG03nJ
-
-## Requirements
-Gradio, ultralytics
-"""
-
-"""## Libraries"""
-
+import PIL.Image as Image
 import gradio as gr
+
 from ultralytics import YOLO
-from PIL import Image
 
-# Function to Load model and Make inference with YOLOv8
-def yolov8_inference(img_path, model_id, conf_threshold, iou_threshold):
-   # Load the model
-    model = YOLO(model_id, conf_threshold, iou_threshold)
+model = YOLO(f"./model/yolov8n.pt")
 
-    # Perform inference
-    results = model(img_path)
+
+def predict_image(img, conf_threshold, iou_threshold):
+    results = model.predict(
+        source=img,
+        conf=conf_threshold,
+        iou=iou_threshold,
+        show_labels=True,
+        show_conf=True,
+        imgsz=640,
+    )
+
     for r in results:
-      im_array = r.plot()  # plot a BGR numpy array of predictions
-      im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
-      im.show()
+        im_array = r.plot()
+        im = Image.fromarray(im_array[..., ::-1])
+
     return im
 
 
-def app():
-    with gr.Blocks():
-        with gr.Row():
-            with gr.Column():
-                img_path = gr.Image(type="filepath", label="Photo")
-                model_name = gr.Dropdown(
-                    label="Model",
-                    value='model/tank_weights.onnx',
-                    choices=[
-                        "model/tank_weights.onnx",
-                        "model/yolov8m.pt",
-                        "yolov8n-seg.pt",
-                        "yolov8m-seg.pt"
-                    ]
-                )
-                conf_threshold = gr.Slider(
-                    label="Confidence Threshold",
-                    minimum=0.1,
-                    maximum=1.0,
-                    step=0.1,
-                    value=0.3
-                )
-                iou_threshold = gr.Slider(
-                    label="IoU Threshold",
-                    minimum=0.1,
-                    maximum=1.0,
-                    step=0.1,
-                    value=0.3
-                )
-                yolo_infer = gr.Button(value="Inference")
+iface = gr.Interface(
+    fn=predict_image,
+    inputs=[
+        gr.Image(type="pil", label="Upload Image"),
+        gr.Slider(minimum=0, maximum=1, value=0.25, label="Confidence threshold"),
+        gr.Slider(minimum=0, maximum=1, value=0.45, label="IoU threshold")
+    ],
+    outputs=gr.Image(type="pil", label="Result"),
+    title="R-CNN Object Detection & Segmentation",
+    description="Upload images for inference. The YOLOv8 model is used with Gradio.",
+    
+)
 
-            with gr.Column():
-                output_numpy = gr.Image(type="numpy",label="Output")
-
-        yolo_infer.click(
-            fn=yolov8_inference,
-            inputs=[
-                img_path,
-                model_name,
-                conf_threshold,
-                iou_threshold
-            ],
-            outputs=[output_numpy],
-        )
-
-
-gradio_app = gr.Blocks()
-with gradio_app:
-    gr.HTML(
-        """
-    <h1 style='text-align: center'>
-    YOLOv8: Object Detection & Segmentation
-    </h1>
-    """)
-    gr.HTML(
-        """
-        <h3 style='text-align: center'>
-          For more information, please refer to
-          <a href='https://kimerared.github.io/R-CNN/' target='_blank'>R-CNN GitHub</a>
-        </h3>
-        """)
-    with gr.Row():
-        with gr.Column():
-            app()
-
-gradio_app.launch(debug=False, show_error=True, max_threads=1)
+if __name__ == '__main__':
+    iface.launch(share=True)
